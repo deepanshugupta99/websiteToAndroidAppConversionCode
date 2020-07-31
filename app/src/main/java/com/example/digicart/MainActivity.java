31023e09044a770e2be575a0d012d25fe5007474
deepanshugupta99 <deepanshugupta717@gmail.com>
2020-07-31 23:37:51 +0530

    first commit

---
+++ app/src/main/java/com/example/digicart/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.digicart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView=findViewById(R.id.webView);
+        WebSettings webSettings=webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.digicart.epizy.com/");
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+}
